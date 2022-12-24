dc09760539168e4a67268310b4de8d7871ce9e05
PatelBala <pbalkrishna221@gmil.com>
2022-12-24 11:57:46 +0530

    mapping

---
+++ Car.java
@@ -0,0 +1,9 @@
+class Car{
+    public void drink(){
+        System.out.println("Drining water");
+    }
+    public static void main(String[] args) {
+        Car c =new Car();
+        c.drink();
+    }
+}
