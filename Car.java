bb5ee7d1b7e5d272586a28e235d308731a3b2da2
PatelBala <pbalkrishna221@gmil.com>
2022-12-26 11:08:11 +0530

    mom

--- Car.java
+++ Car.java
@@ -6,5 +6,11 @@ class Car{
     public static void main(String[] args) {
         Car c =new Car();
         c.drink();
+       
+    }
+    class moter{
+        public void drink(){
+            System.out.println("Drining  water");
+        }
     }
 }
